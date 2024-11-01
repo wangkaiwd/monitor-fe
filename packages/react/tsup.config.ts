2ac3158b40ac4eebd8df74e72612351819061368
wangkaiwd <15049394638Wang>
2024-11-01 15:32:01 +0800

    feat: use tsup bundle library

---
+++ packages/react/tsup.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, Options } from 'tsup'
+
+const createCommonConfig = (options: Options): Options => {
+  return {
+    entry: ['./index.ts'],
+    sourcemap: !!options.watch,
+    clean: true,
+    outExtension({ format }) {
+      return {
+        js: `.${format}.js`,
+      }
+    },
+  }
+}
+
+export default defineConfig((options) => {
+  const commonConfig = createCommonConfig(options)
+  return [
+    {
+      ...commonConfig,
+      format: 'iife',
+      globalName: '__MonitorReact__',
+    },
+    {
+      ...commonConfig,
+      dts: true,
+      format: 'esm'
+    },
+    {
+      ...commonConfig,
+      format: 'cjs'
+    }
+  ]
+})
